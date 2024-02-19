1560591fd90aed4621c7b9a1197e87539f7a9215
AjitUbbanapally <Ajitpurnahk@gmail.com>
2024-02-19 17:14:33 -0600

    Initial Commit

---
+++ gameapp/gameappApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameappApp.swift
+//  gameapp
+//
+//  Created by Ubbanapally,Ajit on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct gameappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
